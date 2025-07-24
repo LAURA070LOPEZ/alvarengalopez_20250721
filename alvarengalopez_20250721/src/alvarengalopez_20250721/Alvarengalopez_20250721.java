3e91e2066408ff27a49c57c48e14bdc6be4c2944
Laura Gisela Alvarenga Lopez <ia.al100123@ufg.edu.sv>
2025-07-23 18:49:00 -0600

    Proyecto

---
+++ alvarengalopez_20250721/src/alvarengalopez_20250721/Alvarengalopez_20250721.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alvarengalopez_20250721;
+
+/**
+ *
+ * @author laura
+ */
+public class Alvarengalopez_20250721 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
